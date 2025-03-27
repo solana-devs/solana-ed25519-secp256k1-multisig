8f19ebdb1507f19c2911969da8166f039c0206b1
rustdev <placex.com@gmail.com>
2025-03-27 21:46:24 +0000

    anchor init

---
+++ tests/multisig.ts
@@ -0,0 +1,16 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { Multisig } from "../target/types/multisig";
+
+describe("multisig", () => {
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.AnchorProvider.env());
+
+  const program = anchor.workspace.Multisig as Program<Multisig>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+    const tx = await program.methods.initialize().rpc();
+    console.log("Your transaction signature", tx);
+  });
+});
